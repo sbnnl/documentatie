9d81e2b9d2b2458a35a5c5b3948c03c8f3b69573
Arjan Geertsema <arjan@synion.nl>
2022-11-09 21:38:01 +0100

    adres toegevoegd en exact pseudo code gewijzigd.

--- docs/100_producten/100_borgstelling/010_aanvragen-borgstelling/RegistreerBorgstellingVerkoopUseCase.java
+++ docs/100_producten/100_borgstelling/010_aanvragen-borgstelling/RegistreerBorgstellingVerkoopUseCase.java
@@ -10,12 +10,13 @@ public class RegistreerBorgstellingVerkoopUseCaseHandler implements UseCaseHandl
     public void handle(RegistreerVerkoopUseCase useCase) 
     {
         var aanvraag = mediator.ask(new GeefBorgstellingAanvraag(useCase.borgstellingId));
+        var borgstellingCategorie = mediator.ask(new GeefBorgstellingCategorie(aanvraag.BorgstellingCategorieId));
+        var kredietbank = mediator.ask(new GeefKredietbank(aanvraag.KredietbankId));
         
         var port = new RegistreerBorgstellingVerkoop();
-        port.BorgstellingId = aanvraag.BorgstellingId;
-        port.KredietbankId = aanvraag.KredietbankId;
-        port.Premie = aanvraag.BrutoKredietsom / 100;;
-        port.Omschrijving = String.format("Borgstelling voor saneringskrediet met kenmerk %s.", aanvraag.Kenmerk);
+        port.Aanvraag = aanvraag;
+        port.BorgstellingCategorie = aanvraag.BorgstellingCategorie;
+        port.Kredietbank = aanvraag.Kredietbank;
 
         mediator.send(port);
     }
@@ -30,18 +31,34 @@ public class ExactService implements PortHandler<RegistreerBorgstellingVerkoop>
     }
 
     public void handle(RegistreerVerkoop port) {
-        var accountId = api.getAccountId(port.KredietbankId);        
-        var itemId =  api.getItemId("Borgstelling"));
+        var accountId = api.getAccountId(port.Kredietbank.KredietbankId);
+        if(accountId == null) {
+            const account = new Account();
+            account.AccountId = port.Kredietbank.KredietbankId;
+            account.Name = port.Kredietbank.Kredietbank.Naam;
+
+            accountId = api.createAccount(account);
+        }
+
+        var itemId =  api.getItemId(port.BorgstellingCategorie.BorgstellingCategorieId));
+        if(itemId == null) {
+            const item = new Item();
+
+            item.ItemId = port.BorgstellingCategorie.BorgstellingCategorieId;
+            item.Name = port.BorgstellingCategorie.Naam;
+
+            itemId = api.createItem(item);
+        }
 
         const salesOrder = new SalesOrder();
         salesOrder.OrderID = port.BorgstellingId;
         salesOrder.OrderedBy = accountId;
-        salesOrder.Description = port.Omschrijving;
+        salesOrder.Description = String.format("Borgstelling voor saneringskrediet met kenmerk %s.", port.Aanvraag.Kenmerk);
 
         const salesOrderLine = new SalesOrderLine();
-        salesOrderLine.AmountFC = port.Premie;
+        salesOrderLine.AmountFC = port.Aanvraag.BrutoKredietsom * port.BorgstellingCategorie.PremiePercentage;;
         salesOrderLine.itemId = itemId;
-        salesOrderLine.Description = port.Omschrijving;
+        salesOrderLine.Description = String.format("Borgstelling %s voor saneringskrediet met kenmerk %s.", port.BorgstellingCategorie.Naam, port.Aanvraag.Kenmerk);
 
         salesOrder.SalesOrderLines = { salesOrderLine };
 
